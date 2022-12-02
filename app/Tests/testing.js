53dfd11f9cd816656e23e55811e72af71814c1b2
Sachin Bhari <sachin.bhari@asterdio.com>
2022-12-02 10:44:09 +0545

    Testing third phase error solve

--- app/Tests/testing.js
+++ app/Tests/testing.js
@@ -3,3 +3,5 @@
 //Looking if tags should be created and then push Code in the Repo and push tag
 "Testing V2.001.01.001"; //Created Next Version Testing
 "Testing V3.001.01.001"; //New Version
+"Testing V3.001.01.001"; //-m There was a problem creating tags before pushing code//
+//Testing if tags should be created after pushing code and then pushing tags
