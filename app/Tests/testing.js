1d2ec65e1b44d7970b3dc8bf5141dfe96f446b25
Sachin Bhari <sachin.bhari@asterdio.com>
2022-12-02 10:48:44 +0545

    Testing Phase fourth

--- app/Tests/testing.js
+++ app/Tests/testing.js
@@ -3,5 +3,6 @@
 //Looking if tags should be created and then push Code in the Repo and push tag
 "Testing V2.001.01.001"; //Created Next Version Testing
 "Testing V3.001.01.001"; //New Version
-"Testing V3.001.01.001"; //-m There was a problem creating tags before pushing code//
+"Testing V3.001.01.002"; //-m There was a problem creating tags before pushing code//
 //Testing if tags should be created after pushing code and then pushing tags
+"Testing V3.001.01.003"; //Error- I forgot to update version and pushed :-) // Created new Breanch testing_2
